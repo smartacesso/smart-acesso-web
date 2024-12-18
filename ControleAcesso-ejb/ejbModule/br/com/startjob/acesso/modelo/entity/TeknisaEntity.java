fab7234c9231fd32cfb63fa7a2411d36f81cd69c
smartacesso <desenvolvimento@smartempresarial.com.br>
2024-12-18 11:46:48 -0300

    Update TeknisaEntity.java

--- ControleAcesso-ejb/ejbModule/br/com/startjob/acesso/modelo/entity/TeknisaEntity.java
+++ ControleAcesso-ejb/ejbModule/br/com/startjob/acesso/modelo/entity/TeknisaEntity.java
@@ -2,19 +2,30 @@ package br.com.startjob.acesso.modelo.entity;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name= "TEKNISA")
 public class TeknisaEntity {
+
 	/*
 	SELECT c.ID_CLIENTE,c.CNPJ, p.nome, a.DATA FROM `controle-acesso`.tb_cliente c
 	left join  `controle-acesso`.tb_pedestre p
 	on c.ID_CLIENTE = p.ID_CLIENTE
 	left join  `controle-acesso`.tb_acesso a
-	on c.ID_CLIENTE = a.ID_CLIENTE;*/
-
+	on c.ID_CLIENTE = a.ID_CLIENTE;
+	
+	*/
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="ID_TEKNISA", nullable=false, length=4)
+	private Long id;
+	
 	private Long numeroDaOrganizacao;
 	private Long codigoDaFilial;
 	
@@ -32,5 +43,6 @@ public class TeknisaEntity {
 		this.codigoDaFilial = codigoDaFilial;
 	}
 
-
+	
 }
+
