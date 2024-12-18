a11af3d7fd4b65ab13c510cc9e50063dea3fc0ad
smartacesso <desenvolvimento@smartempresarial.com.br>
2024-12-18 10:12:31 -0300

    adicionando token

---
+++ ControleAcesso-ejb/ejbModule/br/com/startjob/acesso/modelo/utils/CriptografiaAES.java
@@ -0,0 +1,61 @@
+package br.com.startjob.acesso.modelo.utils;
+
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.spec.IvParameterSpec;
+
+public class CriptografiaAES {
+	private final String IV = "0000000000000000";
+	private final String chaveencriptacao = "0123456789abcdef";
+
+	public String encript(String token) {
+		byte[] textoencriptado = null;
+		try {
+			textoencriptado = encrypt(token, chaveencriptacao);
+			return Base64.getEncoder().encodeToString(textoencriptado);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	public String decript(String textoCriptografado) throws Exception {
+		byte[] decoded = Base64.getDecoder().decode(textoCriptografado);
+		String decript  =  decrypt(decoded, chaveencriptacao);
+		return decript;
+	}
+
+	public byte[] encrypt(String textopuro, String chaveencriptacao) throws Exception {
+		Cipher encripta = Cipher.getInstance("AES/CBC/PKCS5Padding", "SunJCE");
+		SecretKeySpec key = new SecretKeySpec(chaveencriptacao.getBytes("UTF-8"), "AES");
+		encripta.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(IV.getBytes("UTF-8")));
+		return encripta.doFinal(textopuro.getBytes("UTF-8"));
+	}
+
+	public String decrypt(byte[] textoencriptado, String chaveencriptacao) throws Exception {
+		Cipher decripta = Cipher.getInstance("AES/CBC/PKCS5Padding", "SunJCE");
+		SecretKeySpec key = new SecretKeySpec(chaveencriptacao.getBytes("UTF-8"), "AES");
+		decripta.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(IV.getBytes("UTF-8")));
+		return new String(decripta.doFinal(textoencriptado), "UTF-8");
+	}
+
+	public static void main(String[] args) throws Exception {
+	String teste = "TokenOutput [idResponsible=" + 1 + ", login=" + 1 + ", senha=" + 2 + "]";
+		
+		CriptografiaAES criptografiaAES = new CriptografiaAES();
+		byte[] encrypt = criptografiaAES.encrypt(teste, criptografiaAES.chaveencriptacao);
+		
+		String encoded = java.util.Base64.getEncoder().encodeToString(encrypt);
+		System.out.println("Resposta: " + encoded);
+		byte[] decoded = java.util.Base64.getDecoder().decode(encoded);
+		
+		String decript = criptografiaAES.decrypt(decoded, criptografiaAES.chaveencriptacao);
+		
+		System.out.println("Decript: " + decript);
+	}
+
+}
--- ControleAcesso/src/br/com/startjob/acesso/api/TeknisaRequestService.java
+++ ControleAcesso/src/br/com/startjob/acesso/api/TeknisaRequestService.java
@@ -1,16 +1,33 @@
 package br.com.startjob.acesso.api;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import br.com.startjob.acesso.modelo.utils.CriptografiaAES;
+import br.com.startjob.acesso.to.teknisa.TeknisaToken;
+
 @SuppressWarnings("unchecked")
 @Path("/teknisa")
 public class TeknisaRequestService extends BaseService {
+	
+	@Context
+	private HttpServletRequest request;
+	
+	@Context 
+	private HttpServletResponse response;
+	
+	CriptografiaAES cript = new CriptografiaAES();
 	
 	@GET
 	@Path("/login")
@@ -18,6 +35,7 @@ public class TeknisaRequestService extends BaseService {
 	public Response login(@HeaderParam("nome") String nome, @HeaderParam("senha") String senha,
 			@HeaderParam("filial") String filial) {
 		
+		encriptToken(nome, senha, filial);
 		// converter em token
 		// com o cliente achado, fazer o de para no banco com a filial
 		return Response.status(Status.OK).entity("working").build();
@@ -26,11 +44,31 @@ public class TeknisaRequestService extends BaseService {
 	@GET
 	@Path("/access")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response access(@HeaderParam("token") String token) {
+	public Response access(@HeaderParam("token") String token) throws Exception {
 		// converter token
-		
+		decriptToken(token);
 		
 		return Response.status(Status.OK).entity("working").build();
+	}
+	
+	
+	private String encriptToken(final String nome, final String senha, final String filial) {
+		//trocar nome por login
+		TeknisaToken tokenRequest = new TeknisaToken();
+		tokenRequest.setClient(filial);
+		tokenRequest.setSenha(senha);
+		tokenRequest.setNome(nome);
+		Gson gson = new GsonBuilder().create();
+		String token = tokenRequest.toString();
+		return  cript.encript(token);
+	}
+	
+	private TeknisaToken decriptToken(final String token) throws Exception {
+		//colocar classe generica depois
+		Gson gson = new GsonBuilder().create();
+		String tokenR = cript.decript(token);
+		TeknisaToken tokenResponse = gson.fromJson(tokenR, TeknisaToken.class); 
+		return tokenResponse;
 	}
 
 }
