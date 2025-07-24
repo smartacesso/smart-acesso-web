6cfc8848ea6e0954d9911b294e65b3b03a17639c
jordan <jordandimst@gmail.com>
2025-07-24 10:34:25 -0300

    Create TipoTotvsEdu.java

---
+++ ControleAcesso/src/br/com/startjob/acesso/to/TotvsEdu/TipoTotvsEdu.java
@@ -0,0 +1,42 @@
+package br.com.startjob.acesso.to.TotvsEdu;
+
+public enum TipoTotvsEdu {
+	
+	ALUNO("saluno", "ALUNO"),
+	RESPONSAVEL("RESPONSAVELFIN", "RESPONSAVEL"),
+	FUNCIONARIO("PFUNC", "FUNCIONARIO");
+	
+	private String tabelaRm;
+	private String desc;
+	
+	private TipoTotvsEdu(String tabelaRm , String desc) {
+		this.tabelaRm = tabelaRm;
+		this.desc = desc;
+	}
+
+	public String getTabelaRm() {
+		return tabelaRm;
+	}
+
+	public void setTabelaRm(String tabelaRm) {
+		this.tabelaRm = tabelaRm;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	
+    public static TipoTotvsEdu fromTabelaRm(String tabelaRm) {
+        for (TipoTotvsEdu tipo : TipoTotvsEdu.values()) {
+            if (tipo.getTabelaRm().equalsIgnoreCase(tabelaRm)) {
+                return tipo;
+            }
+        }
+        return null;
+    }
+
+}
