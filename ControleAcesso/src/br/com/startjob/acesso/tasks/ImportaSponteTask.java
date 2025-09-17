712f8d80184f78f5e5c1573231428c566038db1d
smartacesso <130687248+smartacesso@users.noreply.github.com>
2025-09-17 20:38:08 -0300

    sponte task

---
+++ ControleAcesso/src/br/com/startjob/acesso/tasks/ImportaSponteTask.java
@@ -0,0 +1,39 @@
+package br.com.startjob.acesso.tasks;
+
+import java.util.TimerTask;
+import java.util.logging.Logger;
+
+import javax.ejb.EJB;
+
+import br.com.startjob.acesso.modelo.ejb.PedestreEJBRemote;
+import br.com.startjob.acesso.services.BaseServlet;
+
+public class ImportaSponteTask extends TimerTask {
+
+	@EJB
+	private PedestreEJBRemote pedestreEJB;
+
+	private Logger logger = Logger.getLogger(this.getClass().getName());
+
+	@Override
+	public void run() {
+		logger.info("Iniciando processo de importação com o Sponte...");
+
+		try {
+			pedestreEJB = ((PedestreEJBRemote) BaseServlet.getEjb(PedestreEJBRemote.class));
+			try {
+				pedestreEJB.importarAD();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			logger.info("Processo de importação do AD finalizado...");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
